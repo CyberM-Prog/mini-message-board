e358c7060e04cad2244cb7c63684d92d758b3d67
Martim <martim.pintao@gmail.com>
2023-07-26 11:38:11 +0100

    Create the routers and views to write and read messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+    res.render("form", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+    const message = req.body;
+
+    messages.push({
+        text: message.message,
+        user: message.author,
+        added: new Date(),
+    });
+
+    res.redirect("/");
 });
 
 module.exports = router;
